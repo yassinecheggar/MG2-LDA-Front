a4c6b0efdb8372f6879562a1389d302200ca45ec
yassinecheggar <cheggar13@gmail.com>
2021-05-28 11:09:40 +0100

    first  commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SignIn from './components/account/SignIn'
+import Dashboard from './components/Dashboard/Dashboard'
+function App() {
+  return (
+    <>
+    
+    <Dashboard/>
+    </>
+  );
+}
+
+export default App;
